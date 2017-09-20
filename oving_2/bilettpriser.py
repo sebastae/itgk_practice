924db61a0d257c738ca93529b222b51893050384
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2017-09-20 12:53:45 +0200

    Finished bilettpriser.py, gave up on OO

--- oving_2/bilettpriser.py
+++ oving_2/bilettpriser.py
@@ -3,29 +3,69 @@
 # Date created: 04.09.17
 # Github: https://github.com/sebastae/itgk_practice/tree/master/oving_2
 
-days_until_depart_str = input("Dager til du skal reise: ")
+days = int(str(input("Dager til du skal reise: ")))
 
-try:
-    days_until_depart_int = int(float(days_until_depart_str))
-except ValueError:
-    print("Get your shit together and give us a number!")
-    exit()
-except:
-    print("Something went horribly wrong... Sorry")
-    exit()
+mp = 199
+fp = 440
 
-if days_until_depart_int >= 14:
+pris = 0
 
-    print("Minipris: 199,- kan ikke refunderes/endres")
-    a = input("ønskes dette? (J/N):")
-    if(a in ('J','j')):
-        print("Takk for pengene! Ha en god reise!")
-    elif(a in ('N','n')):
-        print("Da tilbyr vi fullpris: 440,-")
+def mini():
+    pris = mp
+    cases = {'j':1,'J':1,'n':0,'N':0}
+    ans = input("Er du student? [j/n]: ")
 
-elif days_until_depart_int < 0:
-    print("Woah! We got ourselves a time-traveler here!")
+    if(cases.get(ans,-1) == -1):
+        print("Det du skrev inn ble ikke akseptert, starter på nytt")
+        exit()
+    elif cases.get(ans,0):
+        pris *= 0.9
+    else:
+        pass
+
+    print("\n\nDin bilett koster ",str(pris).format("%0.2f"),",-",sep='')
+    print("Lykke til på reisen!")
+
+def full():
+    pris = fp
+    cases = {'j':1,'J':1,'n':0,'N':0}
+    ans = input("Er du student? [j/n]: ")
+    if(cases.get(ans,-1) == -1):
+        print("Det du skrev inn ble ikke akseptert, starter på nytt")
+        exit()
+    elif cases.get(ans,0):
+        pris *= 0.75
+    else:
+        pass
+    
+    alder = int(input("Hvor gammel er du?: "))
+    
+    if alder<16:
+        pris *= 0.5
+    elif alder>=60:
+        pris *= 0.75
+    else:
+        c = {'j':1,'J':1,'n':0,'N':0}
+        a = input("Er du militær? [j/n]: ")
+        if(c.get(a,-1) == -1):
+            print("Det du skrev inn ble ikke akseptert, starter på nytt")
+            exit()
+        elif(c.get(a,0)):
+            pris*=0.75
+
+    print("\n\nDin bilett koster ",str(pris).format("%0.2f"),",-",sep='')
+    print("Lykke til på reisen!")
+
+if(days <= 14):
+        print("Du kan få minipris (199,-), dette kan ikke endres/refunderes")
+        cases = {'j':1,'J':1,'n':0,'N':0}
+        ans = input("Ønskes dette? [j/n]: ")
+        if(cases.get(ans,-1) == -1):
+            print("Det du skrev inn ble ikke akseptert, starter på nytt")
+            exit()
+        elif cases.get(ans,0):
+            mini()
+        else:
+            full()
 else:
-    print("For sent for minipris: Fullpris 440,-")
-
-
+    full()
--- oving_2/bilettpriser_oo.py
+++ oving_2/bilettpriser_oo.py
@@ -8,3 +8,37 @@ def switch(exp,cases,default):
 def switchval(exp,cases,default):
     return cases.get(exp,default)
 
+ticket = 0
+passenger = 0
+
+class Ticket:
+        def __init__
+
+def failedInput():
+    print("Det du skrev inn ble ikke akseptert")
+    exit()
+
+def btostr(exp):        # Evaluates an expression to either "1" or "0"
+    try:
+        return str(int(bool(exp)))
+    except:
+        print("Could not evaluate expression \"",exp,"\"",sep='')
+
+def Start():
+    try:
+        days = int(input("Dager til du skal reise: "))
+        negd = bool(days > 0)
+        while(negd):
+            negd = False
+            raise IOError
+    except IOError:
+        print("Vi tilbyr desverre ikke tidsreiser...")
+        exit()
+    except:
+        failedInput()
+
+    mf_exp = days <= 14
+    def mf_mini():
+        def nw():
+            ticket = 
+    mf_cases = {'0': }
